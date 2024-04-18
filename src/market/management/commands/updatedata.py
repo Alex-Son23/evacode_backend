52166a6ecf6269c5d8aa05dc19cf98185bb73c76
alex <sashason1990@gmail.com>
2024-04-18 16:02:07 +0300

    repaired prod docker-compose.yml

--- src/market/management/commands/updatedata.py
+++ src/market/management/commands/updatedata.py
@@ -1,13 +1,14 @@
 import time
 from django.core.management.base import BaseCommand, CommandError
 from market.utils import BusinessRuService
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         service = BusinessRuService()
-        
+        print("Script started succesfuly!")
         while True:
             service.api_client.set_token()
             service.group_to_model()
             service.goods_to_model()
-            time.sleep(60*5)
-            
+            time.sleep(60 * 5)
