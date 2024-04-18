d2478c2bf66b8dc664fa954104d555d4372c34af
alex <sashason1990@gmail.com>
2024-04-18 19:06:56 +0300

    add env2

--- src/market/management/commands/updatedata.py
+++ src/market/management/commands/updatedata.py
@@ -1,4 +1,6 @@
 import time
+
+import requests
 from django.core.management.base import BaseCommand, CommandError
 from market.utils import BusinessRuService
 
@@ -7,6 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Script started succesfuly!")
         service = BusinessRuService()
+        requests.get('google.com')
         while True:
             service.api_client.set_token()
             service.group_to_model()
